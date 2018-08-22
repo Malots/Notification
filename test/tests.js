f935d9bebdba3c643ef59734d7090f100bd770a1
leovene <leo.ochoalopes@gmail.com>
2018-08-21 22:47:40 -0300

    Update testes

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,24 @@
 import Notification from '../src/Notification';
 import test from 'tape';
 
-const notify = new Notification();
+class Tests extends Notification {
+    constructor(valueOne,valueTwo) {
+        super(valueOne,valueTwo);
+    }
+}
+
+const notify = new Tests(10,'value');
 
 test('Deve retornar que não é válido se possuir algum erro', function(t) {
     notify.addError('Erro','Valor');
+    t.plan(1);
     t.equal(notify.isValid(),false);
     t.end();
 });
 
 test('Deve retornar o valor do erro', function(t) {
     notify.addError('Erro','Valor');
+    t.plan(1);
     t.equal(notify.getError('Erro'),'Valor');
     t.end();
 });
@@ -19,18 +27,21 @@ test('Deve adicionar apenas uma chave igual ao erro', function(t) {
     notify.addError('Erro','Valor um');
     notify.addError('Erro','Valor dois');
     notify.addError('Erro','Valor três');
+    t.plan(1);
     t.equal(notify.countErrors(),1);
     t.end();
 });
 
 test('Deve retornar chave não encontrada', function(t) {
     notify.addError('Erro','Valor');
+    t.plan(1);
     t.equal(notify.getError('Chave'),'Key not found');
     t.end();
 });
 
 test('Deve retornar um json com os erros', function(t) {
     notify.addError('Erro','Valor');
+    t.plan(1);
     t.isEquivalent(notify.getAllErrors(),{ 0: { error: 'Erro', message: 'Valor' } });
     t.end();
 });
